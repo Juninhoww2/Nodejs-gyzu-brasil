873c0a9de5ede029ec2537da12347b6e12e62f33
Juninhoww2 <alqadianoKira@gmail.com>
2021-11-22 17:32:38 -0300

    conn.js

---
+++ db/conn.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require('mongodb')
+
+const uri = 'mongodb://localhost:27017/testemongodb2'
+
+const client = new MongoClient(uri)
+
+async function run() {
+    try {
+        await client.connect()
+        console.log('Conectado ao MongoDB!') 
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+run()
